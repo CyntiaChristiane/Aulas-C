7900bb9706e1bec36764d55d18c409ef4cde2bf1
CyntiaChristiane <143567151+CyntiaChristiane@users.noreply.github.com>
2023-08-29 21:57:34 -0400

    Add files via upload

--- Exercicio_1/src/main.c
+++ Exercicio_1/src/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+// Salvar informação digitada do teclado na variável
+/**
+ * %d = int (número inteiro)
+ * %f = float (número real)
+ * %c = char (um caractere)
+ * %s = char[] (um texto inteiro em uma única linha)
+ * 
+ * Exemplo número inteiro:
+ * & indica que é uma referência a uma variável
+ * scanf("%d", &nomeDaVariável);
+*/
+
 int main()
 {
-	printf("Hello World!\n");
+	// Declaração de números inteiros
+	int n1, n2;
+
+	// escrever na tela
+	printf("Informe o primeiro numero: ");
+	scanf("%d", &n1);
+
+	printf("Informe o segundo numero: ");
+	scanf("%d", &n2);
+
+	// Se n1 > n2
+	if(n1>n2){
+		// Executa esse código quando for verdadeiro
+		printf("O menor numero e: %d", n2);
+	} else {
+		// Executa esse quando for falso
+		printf("O menor numero e: %d", n1);
+	}
 
 	return (0);
 }
--- Exercicio_2/src/main.c
+++ Exercicio_2/src/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 
+/**
+ * Condicional é sempre assim: if (expressão de comparação) { código }
+ * 
+ * Exemplo:
+ * Se x é maior ou igual que 5 e menor que 10 então mostre 'X é maior ou igual a 5 e menor que 10'
+ * 
+ * int x = 6;
+ * if (x >= 5 && x < 10) {
+ * 	printf("X é maior ou igual a 5 e menor que 10");
+ * }
+ * 
+ * Caso contrário se x é maior que 10 então diga 'X é maior que 10'
+ * if (x >= 5 && x < 10) {
+ * 	printf("X é maior ou igual a 5 e menor que 10");
+ * } else if (x > 10) {
+ * 	printf("X é maior que 10");
+ * }
+*/
+
 int main()
 {
-	printf("Hello World!\n");
+	int n1, n2, n3;
+
+	printf("Informe o primeiro numero: ");
+	scanf("%d", &n1);
+
+	printf("Informe o segundo numero: ");
+	scanf("%d", &n2);
+
+	printf("Informe o terceiro numero: ");
+	scanf("%d", &n3);
+
+	if(n1>n2 && n1>n3) {
+		printf("O maior numero e: %d", n1);
+	} else if (n2>n1 && n2>n3) {
+		printf("O maior numero e: %d", n2);
+	} else {
+		printf("O maior numero e: %d", n3);
+	}
 
 	return (0);
 }
